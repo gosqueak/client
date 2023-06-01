064afb5b8d37d860df1fce1cc80e88f541e1c94a
rebeljah <rebeljah@gmail.com>
2023-06-01 01:16:57 -0400

    add global user object after login

--- static/login.js
+++ static/login.js
@@ -1,5 +1,5 @@
 import { passwordLogin } from "./auth.js";
-import { STATIC_SERV } from "./constants.js";
+import { LoggedInUser } from "./models.js";
 
 const form = document.getElementById("login-form");
 form.addEventListener("submit", async function (event) {
@@ -7,15 +7,12 @@ form.addEventListener("submit", async function (event) {
 
     const username = document.getElementById("username");
     const password = document.getElementById("password");
-
-    const user = username.value;
-    const pass = password.value;
-
-    password.value = username.value = "";
-
+    
     // catch http error codes from login request
     try {
-        await passwordLogin(user, pass);
+        const userID = await passwordLogin(username.value, password.value);
+        window.loggedInUser = new LoggedInUser(userID, username.value, password.value);
+        password.value = username.value = "";
     } catch (error) {
         if (error.status == 401) {
             alert("Incorrect username or password");
